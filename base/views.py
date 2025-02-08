05483ced233280a21ee15fc4248def7dc31a915a
Samuel Obeng <obengsamuel270@gmail.com>
2025-02-08 21:38:56 +0000

    created login

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Room,Topic
 from .forms import RoomForm
 
@@ -11,6 +14,26 @@ from .forms import RoomForm
 # ]
 
 # Create your views here.
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print(f"Attempting login for: {username}")  # Debugging
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            print(f"User authenticated: {user}")  # Debugging
+            login(request, user)
+            return redirect('home')
+        else:
+            print("Authentication failed")  # Debugging
+            messages.error(request, 'Username OR password is incorrect')
+
+    return render(request, 'base/login_registration.html')
+
 def home(request):
     q= request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) & Q(name__icontains=q) | Q(description__icontains=q))
